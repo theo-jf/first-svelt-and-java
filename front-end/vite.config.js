27387861a30954cd1d919bc99b9cb9ab4ccd363c
Theo Janke-Furman <tjankefurman@gmail.com>
2022-10-29 15:26:06 -0500

    Configured localhost port

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import dns from 'dns'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [svelte()]
+// })
+
+// Allows 'localhost' to be default address
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
-  plugins: [svelte()]
+  // Makes localhost run on port 5000
+  server: {
+    port: 5000
+  }
 })
