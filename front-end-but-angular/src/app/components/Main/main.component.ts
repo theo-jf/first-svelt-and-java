afd84e8efd87e77e2209db96b219ee3aac9ae7a1
Theo Janke-Furman <tjankefurman@gmail.com>
2023-01-02 14:04:53 -0600

    Converted functions to typescript

--- front-end-but-angular/src/app/components/Main/main.component.ts
+++ front-end-but-angular/src/app/components/Main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'new-main',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
   title = 'myMain';
+
+  // UseEffect knock-off
+  set foo(rocks: number) {
+    // Do something here
+  }
+
+
+  response = 'Press the button!';
+  addRock = async () => {
+      new Audio('./l-e5pazuT1o_48.mp3').play();
+    try {
+      let addMessage = await axios.post('api/server/add');
+      console.log(addMessage.data);
+      fetchRocks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  rocks = 0;
+  fetchRocks = async (rocks: number, response: string): Promise<null> => {
+    try {
+      let serverResponse = await axios.get('api/server/fetch');
+      rocks = serverResponse.data;
+      response = `${rocks} rocks`;
+    } catch (error) {
+      console.log(error);
+      response = `The server doesn't appear to be connected ...`;
+    }
+    return null;
+  };
 }
